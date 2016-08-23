cfd98e957fa3670bda1ff219daa294c5e7a8402a
Hamza Khan <hamzakhan@Fatimas-MacBook-Pro-2.local>
2016-08-23 17:37:24 -0400

    added new contacts

--- crm.rb
+++ crm.rb
@@ -2,9 +2,26 @@
 # Do NOT copy the CRM class from the old crm assignment, as it won't work at all for the web-based version!
 # You'll have to implement it from scratch.
 
+
+require_relative "contact"
 require 'sinatra'
 
+# Fake data
+Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
+Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
+Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
+
+
 get '/' do
-  @crm_app_name = "Hamza CRM"
+  @crm_app_name = "CRM, Hamza!"
   erb :index
+end
+
+get '/contacts' do
+  @contacts
+  erb :contacts
+end
+
+get '/contacts/new' do
+  erb :new_contact
 end
