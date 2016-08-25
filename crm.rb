70aae56e11ccac6f0bed137114110e1ce1f5b80c
Hamza Khan <hamzakhan@Fatimas-MacBook-Pro-2.local>
2016-08-25 16:13:16 -0400

    generalized contacts

--- crm.rb
+++ crm.rb
@@ -8,12 +8,9 @@ require 'sinatra'
 
 # Temporary fake data so that we always find contact with id 1.
 Contact.create('Johnny', 'Bravo', 'johnny@bitmakerlabs.com', 'Rockstar Games')
-
-
-# # Fake data
-# Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
-# Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
-# Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
+Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
+Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
+Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
 
 
 get '/' do
@@ -35,7 +32,7 @@ post '/contacts' do
   redirect to('/contacts')
 end
 
-get '/contacts/1' do
-  @contact = Contact.find(1)
+get '/contacts/:id' do
+  @contact = Contact.find(params[:id].to_i)
   erb :show_contact
 end
