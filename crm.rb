6899f1fcabc3f66ce6faec5f715f935f05d708d9
Hamza Khan <hamzakhan@Fatimas-MacBook-Pro-2.local>
2016-08-25 18:13:17 -0400

    added contactedit

--- crm.rb
+++ crm.rb
@@ -32,11 +32,20 @@ post '/contacts' do
   redirect to('/contacts')
 end
 
+get '/contacts/:id/edit' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
 get '/contacts/:id' do
   @contact = Contact.find(params[:id].to_i)
-if @contact
-  erb :show_contact
-else
-  raise Sinatra::NotFound
-end
+  if @contact
+    erb :show_contact
+  else
+    raise Sinatra::NotFound
+  end
 end
